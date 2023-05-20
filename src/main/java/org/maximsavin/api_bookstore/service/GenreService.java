95ceceef30facc8bcfe35c7973964e5e55590cef
Maxim21S <maximsavin21@gmail.com>
2023-05-20 15:15:21 +0300

    Create GenreService.java

---
+++ src/main/java/org/maximsavin/api_bookstore/service/GenreService.java
@@ -0,0 +1,20 @@
+package org.maximsavin.api_bookstore.service;
+
+import org.maximsavin.api_bookstore.model.Genre;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public interface GenreService {
+
+    List<Genre> getAll();
+
+    Genre getById(long id);
+
+    Genre create(Genre genre);
+
+    Genre update(Genre genre);
+
+    boolean deleteById(long id);
+}
