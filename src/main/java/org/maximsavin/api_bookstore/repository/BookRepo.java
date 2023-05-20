bf2a4b958e87c44a9dc53686adfd8a09b60eb678
Maxim21S <maximsavin21@gmail.com>
2023-05-20 14:54:09 +0300

    Create BookRepo.java

---
+++ src/main/java/org/maximsavin/api_bookstore/repository/BookRepo.java
@@ -0,0 +1,9 @@
+package org.maximsavin.api_bookstore.repository;
+
+import org.maximsavin.api_bookstore.model.Book;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface BookRepo extends JpaRepository<Book, Long> {
+}
