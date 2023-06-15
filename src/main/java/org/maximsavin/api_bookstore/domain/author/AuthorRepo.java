10efb9233a51c27d80afaf80823819e24bd3022e
Maxim21S <maximsavin21@gmail.com>
2023-06-15 21:36:07 +0300

    Update AuthorRepo.java

--- src/main/java/org/maximsavin/api_bookstore/domain/author/AuthorRepo.java
+++ src/main/java/org/maximsavin/api_bookstore/domain/author/AuthorRepo.java
@@ -3,6 +3,12 @@ package org.maximsavin.api_bookstore.domain.author;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Set;
+
 @Repository
 public interface AuthorRepo extends JpaRepository<Author, Long> {
+
+    Author findByName(String name);
+
+    Set<Author> findAuthorsByNameIn(Set<String> names);
 }
