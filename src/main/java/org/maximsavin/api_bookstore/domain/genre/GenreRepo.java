5dc29f2124f9821379be97151b5189948b4dd77f
Maxim21S <maximsavin21@gmail.com>
2023-06-15 21:36:12 +0300

    Update GenreRepo.java

--- src/main/java/org/maximsavin/api_bookstore/domain/genre/GenreRepo.java
+++ src/main/java/org/maximsavin/api_bookstore/domain/genre/GenreRepo.java
@@ -3,7 +3,14 @@ package org.maximsavin.api_bookstore.domain.genre;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Set;
+
 @Repository
 public interface GenreRepo extends JpaRepository<Genre, Long> {
+
     boolean existsByName(String name);
+
+    Genre findByName(String name);
+
+    Set<Genre> findGenresByNameIn(Set<String> names);
 }
